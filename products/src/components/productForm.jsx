0fb06203d84d3382bc8f55a51794beb2b1ae7d96
Aman Khan <amankhan280401@gmail.com>
2026-01-03 17:07:54 +0530

    fevicon

--- products/src/components/productForm.jsx
+++ products/src/components/productForm.jsx
@@ -80,7 +80,7 @@ const ProductForm = ({ onSubmit, editingProduct, isSaving }) => {
         setForm({ ...form, description: e.target.value })
       }
       className="w-full rounded-lg border border-slate-300 px-3 py-2 text-sm
-        focus:outline-none focus:ring-1 focus:ring-black resize-none"
+        focus:outline-none focus:ring-1 focus:ring-black resize"
     />
   </div>
 
